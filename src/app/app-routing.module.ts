bb28a7f285e3e207627695f8f37700922aaf2e6d
Alexandre Topsi <alexandre.topsi@etu.unilim.fr>
2020-12-16 11:30:12 -0500

    partie 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+     { path: 'detail/:id', component: HeroDetailComponent },
+     { path: 'heroes', component: HeroesComponent },
+     { path: 'dashboard', component: DashboardComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
