d49e5a2d51d3ea51b3f15ebb4ca734b644252a17
Alexandre Topsi <alexandre.topsi@etu.unilim.fr>
2020-12-15 23:24:11 -0500

    partie 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/Hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,18 +8,16 @@ import { Hero } from '../interfaces/Hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-    
-    
 
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-      };
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+    heroes = HEROES;
+    selectedHero: Hero;
+  
+    constructor() { }
+  
+    ngOnInit() {
+    }
+  
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
   }
-
-}
